6ec99bb6f77f24d84cca3567060abf96ee9e4c2b
tec-Ity <tecitys@gmail.com>
2021-08-13 17:42:31 +0200

    add order redux

--- src/redux/order/orderSlice.js
+++ src/redux/order/orderSlice.js
@@ -1,13 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetch_Prom } from "../../api";
 
 const initialState = {
   orders: [],
+  orderStatus: "idle",
 };
+
+const populateObjs = [
+  {
+    path: "OrderProds",
+    select: "OrderSkus nome unit",
+    populate: { path: "OrderSkus", select: "price" },
+  },
+];
+
+export const fetchOrders = createAsyncThunk(
+  "order/fetchOrders",
+  async ({ queryURL, isReload = false }, { getState }) => {
+    const Orders_res = await fetch_Prom(
+      "/Orders?populateObjs=" + JSON.stringify(populateObjs) + queryURL
+    );
+    // console.log(Orders_res)
+    if (Orders_res.status === 200) {
+      if (isReload) {
+        return Orders_res.data.objects;
+      } else {
+        return [...getState.orders, ...Orders_res.data.objects];
+      }
+    } else {
+      console.log("Orders", Orders_res.message);
+    }
+  }
+);
 
 export const orderSlice = createSlice({
   name: "order",
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchOrders.pending]: (state, action) => {
+      state.orderStatus = "loading";
+    },
+    [fetchOrders.fulfilled]: (state, action) => {
+      state.orderStatus = "succeed";
+      state.orders = action.payload;
+    },
+    [fetchOrders.rejected]: (state, action) => {
+      state.orderStatus = "error";
+    },
+  },
 });
+
+
 
 export default orderSlice.reducer;
--- src/view/order/OrdersPage.js
+++ src/view/order/OrdersPage.js
@@ -1,31 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { fetch_Prom } from "../../api";
+import { fetchOrders } from "../../redux/order/orderSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function OrdersPage() {
-        const [Orders, setOrders] = useState([]);
-        const getOrders = async(queryObj, isReload=false) => {
-                try {
-                        const populateObjs = [{path: "OrderProds", select: "OrderSkus nome unit", populate: {path: "OrderSkus", select: "price"}}];
-                        const Orders_res = await fetch_Prom("/Orders?populateObjs="+JSON.stringify(populateObjs));
-                        // console.log(Orders_res)
-                        if(Orders_res.status === 200) {
-                                if(isReload) {
-                                        setOrders(Orders_res.data.objects);
-                                } else {
-                                        setOrders([...Orders, ...Orders_res.data.objects]);
-                                }
-                        } else {
-                                console.log("Orders", Orders_res.message)
-                        }
-                } catch(error) {
-                        console.log("error: ", error);
-                }
-        }
-        useEffect(() => {
-                getOrders(null, false);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, []);
-        return (<>
-                
-        </>)
+  const dispatch = useDispatch();
+  const orders = useSelector((state) => state.order.orders);
+  const orderStatus = useSelector((state) => state.order.orderStatus);
+
+  const getOrders = async (queryURL = "", isReload = false) => {
+    if (orderStatus === "idle") {
+      dispatch(fetchOrders(queryURL, isReload));
+    }
+  };
+  useEffect(() => {
+    getOrders();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <></>;
 }
